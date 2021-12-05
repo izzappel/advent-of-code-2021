c5288f4df7bc049beed9b25813cd49b3f20e57de
Isabel Boos <iboos@tocco.ch>
2021-12-05 10:34:47 +0100

    day05

---
+++ src/day05/part1.js
@@ -0,0 +1,103 @@
+const { readInput } = require('../file')
+
+const getTestInput = () => readInput(`${__dirname}/test-input.txt`)
+
+const getInput = () => readInput(`${__dirname}/input.txt`)
+
+const getStartCoordinates = (a, b) => {
+  if (a.x !== b.x && a.y !== b.y) {
+    return a
+  }
+
+  if (a.x === b.x) {
+    const startY = Math.min(a.y, b.y)
+    return { x: a.x, y: startY }
+  }
+
+  const startX = Math.min(a.x, b.x)
+  return { x: startX, y: a.y }
+}
+
+const getEndCoordinates = (a, b) => {
+  if (a.x !== b.x && a.y !== b.y) {
+    return b
+  }
+
+  if (a.x === b.x) {
+    const endY = Math.max(a.y, b.y)
+    return { x: a.x, y: endY }
+  }
+
+  const endX = Math.max(a.x, b.x)
+  return { x: endX, y: a.y }
+}
+
+const extractCoordinates = (line) => {
+  const splitted = line
+    .split(' -> ')
+    .map((n) => n.split(','))
+    .map((n) => ({
+      x: parseInt(n[0], 10),
+      y: parseInt(n[1], 10),
+    }))
+  const a = splitted[0]
+  const b = splitted[1]
+
+  return {
+    start: getStartCoordinates(a, b),
+    end: getEndCoordinates(a, b),
+  }
+}
+
+const getLinePoints = (line) => {
+  if (line.start.x === line.end.x) {
+    const numberOfPoints = line.end.y - line.start.y + 1
+    const points = Array.from(
+      { length: numberOfPoints },
+      (_, i) => i + line.start.y
+    )
+    return points.map((p) => ({ x: line.start.x, y: p }))
+  }
+
+  const numberOfPoints = line.end.x - line.start.x + 1
+  const points = Array.from(
+    { length: numberOfPoints },
+    (_, i) => i + line.start.x
+  )
+  return points.map((p) => ({ x: p, y: line.start.y }))
+}
+
+const isStraightLine = (line) =>
+  line.start.x === line.end.x || line.start.y === line.end.y
+
+const main = (data) => {
+  const lines = data.map(extractCoordinates).filter(isStraightLine)
+
+  /**
+   * {'0:0': 1, '1:2': 1}
+   */
+  const map = lines.reduce((acc, line) => {
+    const points = getLinePoints(line)
+    return {
+      ...acc,
+      ...points.reduce((sum, point) => {
+        const key = `${point.x}:${point.y}`
+        return {
+          ...sum,
+          [key]: (acc[key] || 0) + 1,
+        }
+      }, {}),
+    }
+  }, {})
+
+
+  const dangerousPoints = Object.values(map).filter((n) => n > 1).length
+  console.log(dangerousPoints)
+  return dangerousPoints
+}
+
+module.exports = {
+  getInput,
+  getTestInput,
+  main,
+}
---
+++ src/day05/part2.js
@@ -0,0 +1,74 @@
+const { readInput } = require('../file')
+
+const getTestInput = () => readInput(`${__dirname}/test-input.txt`)
+
+const getInput = () => readInput(`${__dirname}/input.txt`)
+
+const extractCoordinates = (line) => {
+  const splitted = line
+    .split(' -> ')
+    .map((n) => n.split(','))
+    .map((n) => ({
+      x: parseInt(n[0], 10),
+      y: parseInt(n[1], 10),
+    }))
+  const start = splitted[0]
+  const end = splitted[1]
+
+  return {
+    start,
+    end,
+  }
+}
+
+const add = (a, b) => a + b
+const sub = (a, b) => b - a
+const eq = (a, b) => b
+
+const createPoints = (numberOfPoints, start, end) => {
+  const fn = start == end ? eq : start > end ? sub : add
+  const points = Array.from({ length: numberOfPoints }, (_, i) => fn(i, start))
+  return points
+}
+
+const getLinePoints = (line) => {
+  const numberOfXPoints = Math.abs(line.end.x - line.start.x) + 1
+  const numberOfYPoints = Math.abs(line.end.y - line.start.y) + 1
+  const numberOfPoints = Math.max(numberOfXPoints, numberOfYPoints)
+  const xPoints = createPoints(numberOfPoints, line.start.x, line.end.x)
+  const yPoints = createPoints(numberOfPoints, line.start.y, line.end.y)
+
+  const linePoints = xPoints.map((x, index) => ({ x: x, y: yPoints[index] }))
+  return linePoints
+}
+
+const main = (data) => {
+  const lines = data.map(extractCoordinates)
+
+  /**
+   * {'0:0': 1, '1:2': 1}
+   */
+  const map = lines.reduce((acc, line) => {
+    const points = getLinePoints(line)
+    return {
+      ...acc,
+      ...points.reduce((sum, point) => {
+        const key = `${point.x}:${point.y}`
+        return {
+          ...sum,
+          [key]: (acc[key] || 0) + 1,
+        }
+      }, {}),
+    }
+  }, {})
+
+  const dangerousPoints = Object.values(map).filter((n) => n > 1).length
+  console.log(dangerousPoints)
+  return dangerousPoints
+}
+
+module.exports = {
+  getInput,
+  getTestInput,
+  main,
+}
