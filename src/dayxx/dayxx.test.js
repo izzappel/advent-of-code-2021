a5670edaf329e58ead2ceeeea429647ce65b4a05
Isabel Boos <iboos@tocco.ch>
2021-12-05 08:07:49 +0100

    template

---
+++ src/dayxx/dayxx.test.js
@@ -0,0 +1,32 @@
+const part1 = require('./part1')
+const part2 = require('./part2')
+
+describe('dayxx', () => {
+  describe('part1', () => {
+    test('should handle test input', async () => {
+      const data = await part1.getTestInput()
+      const result = part1.main(data)
+      expect(result).toEqual(0)
+    })
+
+    test('should handle real input', async () => {
+      const data = await part1.getInput()
+      const result = part1.main(data)
+      expect(result).toEqual(0)
+    })
+  })
+
+  describe('part2', () => {
+    test('should handle test input', async () => {
+      const data = await part2.getTestInput()
+      const result = part2.main(data)
+      expect(result).toEqual(0)
+    })
+
+    test('should handle real input', async () => {
+      const data = await part2.getInput()
+      const result = part2.main(data)
+      expect(result).toEqual(0)
+    })
+  })
+})
---
+++ src/dayxx/part1.js
@@ -0,0 +1,15 @@
+const { readInput } = require('../file')
+
+const getTestInput = () => readInput(`${__dirname}/test-input.txt`)
+
+const getInput = () => readInput(`${__dirname}/input.txt`)
+
+const main = (data) => {
+  return 0
+}
+
+module.exports = {
+  getInput,
+  getTestInput,
+  main,
+}
